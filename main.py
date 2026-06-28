cf54a7093efd5279b992f7786809fd08caa230d2
vbenz-eng <151744795+vbenz-eng@users.noreply.github.com>
2026-06-28 15:49:46 +0300

    This is an Odd/Even number identifier

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-printHello World
+
+
+numbers = [int(input("Enter number: "))]
+
+for number in numbers:
+    if  isinstance(number, (int,float)):
+        if number % 2 == 1:
+            print(f'{number}, is Odd')
+        elif number % 2 == 0:
+            print(f'{number}, is Even')
+        else:
+            print("invalid input")
+    else:
+        print("invalid input")
--- main1.py
+++ main1.py
@@ -1,1 +1,16 @@
-printHello World
+
+def number_checker(numbers):
+    if isinstance(numbers,(int,float)):
+        raise ValueError('Invalid input')
+    else:
+        for index,number in enumerate(numbers, start=1):
+            if number % 2 == 1:
+                print(f'{index}. {number}, is Odd')
+            elif number % 2 == 0:
+                print(f'{index}. {number}, is Even')
+            else:
+                print("invalid input")
+num_input = int(input("Enter a number: "))
+list = [4, 3, 2, 1]
+list.append(num_input)
+number_checker(list)
