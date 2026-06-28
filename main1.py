8c2e369b83b6b8711c31f39412629cbc2257960f
vbenz-eng <151744795+vbenz-eng@users.noreply.github.com>
2026-06-28 16:19:58 +0300

    Updated the number_checker function to handle both single numbers and lists of numbers, and modified the input handling to accept comma-separated numbers.

--- main1.py
+++ main1.py
@@ -1,16 +1,16 @@
 
 def number_checker(numbers):
     if isinstance(numbers,(int,float)):
-        raise ValueError('Invalid input')
-    else:
-        for index,number in enumerate(numbers, start=1):
-            if number % 2 == 1:
-                print(f'{index}. {number}, is Odd')
-            elif number % 2 == 0:
-                print(f'{index}. {number}, is Even')
-            else:
-                print("invalid input")
-num_input = int(input("Enter a number: "))
-list = [4, 3, 2, 1]
-list.append(num_input)
-number_checker(list)
+        numbers = [numbers]
+
+    for index,number in enumerate(numbers, start=1):
+        if number % 2 == 1:
+            print(f'{index}. {number}, is Odd')
+        elif number % 2 == 0:
+            print(f'{index}. {number}, is Even')
+        else:
+            print("invalid input")
+num_input = input("Enter comma separated numbers: ")
+saved_numbers = [int(x.strip()) for x in num_input.split(',')]
+
+number_checker(saved_numbers)
